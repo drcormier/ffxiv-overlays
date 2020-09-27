76b01e11c14ef4e246b2a6620033793a0fcc4a58
Daniel Cormier <dcormier@rochester.rr.com>
2020-09-27 16:17:58 -0400

    added party iLVL checker

--- rangeFinder/rangeFinder.js
+++ rangeFinder/rangeFinder.js
@@ -3,6 +3,33 @@ let playerY = 0
 let rangeInner = 15
 let rangeOuter = 30
 
+let partyILVLS = {}
+
+function partyILVL (event) {
+  for (const partyMember of event['party']) {
+    let name = partyMember['name']
+    partyILVLS[name] = sendRequest('localhost:6969', { 'name': name })['ilvl']
+  }
+}
+
+function formatParams (params) {
+  return '?' + Object.keys(params).map(function (key) {
+    return key + '=' + encodeURIComponent(params[key])
+  }).join('&')
+}
+
+function sendRequest (url, payload) {
+  let xmlhttp = new XMLHttpRequest()
+  let data = {}
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      data = JSON.parse(this.responseText)
+    }
+  }
+  xmlhttp.open('GET', url + formatParams(payload), false)
+  xmlhttp.send()
+  return data
+}
 
 function coordinateDistanceToPlayer (x, y) {
   return Math.sqrt(Math.pow(x - playerX, 2) + Math.pow(y - playerY, 2))
@@ -24,7 +51,8 @@ function combatantTimer (partyDiv) {
       const partyMemberY = partyMember['PosY']
       const distance = coordinateDistanceToPlayer(partyMemberX, partyMemberY)
       let div = document.createElement('div')
-      div.textContent = `${partyMember['Name']} - ${distance.toFixed(1)}y`
+      div.textContent = `[${partyILVLS[partyMember['Name']]}] ${partyMember['Name']} - ${distance.toFixed(
+        1)}y`
       div.style.color = 'red'
       if (distance <= rangeOuter) div.style.color = 'yellow'
       if (distance <= rangeInner) div.style.color = 'green'
@@ -43,39 +71,42 @@ function updateRangeInner (slider) {
   try {
     rangeInner = slider.target.value
   } catch (e) { }
-  document.querySelector(".rangeInnerText").textContent = `Range: ${rangeInner}`
+  document.querySelector('.rangeInnerText').textContent = `Range: ${rangeInner}`
 }
+
 function updateRangeOuter (slider) {
   try {
     rangeOuter = slider.target.value
   } catch (e) { }
-  document.querySelector(".rangeOuterText").textContent = `Range: ${rangeOuter}`
+  document.querySelector('.rangeOuterText').textContent = `Range: ${rangeOuter}`
 }
 
-function hideSettings(...args){
+function hideSettings (...args) {
   for (const arg of args) {
     arg.style.display = 'none'
   }
 }
 
-function hideButton(event){
+function hideButton (event) {
   const rangeInnerSlider = document.querySelector('#rangeInner')
   const rangeOuterSlider = document.querySelector('#rangeOuter')
-  const rangeInnerText = document.querySelector(".rangeInnerText")
-  const rangeOuterText = document.querySelector(".rangeOuterText")
-  const buttonHide = document.querySelector(".buttonHide")
-  hideSettings(rangeInnerSlider, rangeOuterSlider, rangeInnerText, rangeOuterText, buttonHide)
+  const rangeInnerText = document.querySelector('.rangeInnerText')
+  const rangeOuterText = document.querySelector('.rangeOuterText')
+  const buttonHide = document.querySelector('.buttonHide')
+  hideSettings(rangeInnerSlider, rangeOuterSlider, rangeInnerText,
+    rangeOuterText, buttonHide)
 }
 
 window.addEventListener('DOMContentLoaded', async (_) => {
   const partyDiv = document.querySelector('.party')
   const rangeInnerSlider = document.querySelector('#rangeInner')
   const rangeOuterSlider = document.querySelector('#rangeOuter')
-  const buttonHide = document.querySelector(".buttonHide")
-  addOverlayListener("onPlayerChangedEvent", (e) => {
+  const buttonHide = document.querySelector('.buttonHide')
+  addOverlayListener('onPlayerChangedEvent', (e) => {
     playerX = e['detail']['pos']['x']
     playerY = e['detail']['pos']['y']
-  });
+  })
+  addOverlayListener('PartyChanged', partyILVL)
   rangeInnerSlider.addEventListener('change', updateRangeInner)
   updateRangeInner(rangeInnerSlider)
   rangeOuterSlider.addEventListener('change', updateRangeOuter)
